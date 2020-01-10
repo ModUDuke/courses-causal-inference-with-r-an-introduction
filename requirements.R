5e20babc715b7390274ae7e21915f8ec7513e020
ModUDuke <modu.duke@gmail.com>
2020-01-09 19:12:43 -0500

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -3,6 +3,7 @@ library(remotes)
 ##### Install specific package versions from CRAN #####
 
 install_version("ggplot2", "3.0.0")
+install_version("dplyr", "0.8.3")
 
 # install_version("tidyr", "0.8.1")
 # install_version("purrr", "0.2.5")
